001c7d554aed15f8cc0fcc980fea9eae20e15576
Hồ Quang Sang <sangdinhban@gmail.com>
2026-06-10 17:43:51 +0700

    feat: add Prisma database foundation

---
+++ packages/database/src/index.ts
@@ -0,0 +1,15 @@
+export {
+  DatabaseClientProvider,
+  createDatabaseClient,
+  type DatabaseClient,
+  type DatabaseTransaction,
+} from './client';
+export {
+  ExamTrack,
+  Prisma,
+  type Candidate,
+  type CandidateScore,
+  type Exam,
+  type ForeignLanguage,
+  type Subject,
+} from './generated/prisma/client';
